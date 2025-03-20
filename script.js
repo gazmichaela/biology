7e9a817466e71e5555eac12b300c225b8192091e
gazmichaela <michaela09g@gmail.com>
2025-03-20 15:24:48 +0100

    Popis změn

---
+++ script.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let dropdownToggle = document.querySelector(".dropdown-toggle");
+    let dropdownContent = document.querySelector(".dropdown-content");
+
+    let hideTimeout;
+
+    dropdownToggle.addEventListener("mouseenter", function () {
+        clearTimeout(hideTimeout);
+        dropdownContent.style.display = "block";
+        dropdownContent.style.opacity = "1";
+        dropdownContent.style.visibility = "visible";
+    });
+
+    dropdownContent.addEventListener("mouseenter", function () {
+        clearTimeout(hideTimeout);
+    });
+
+    dropdownToggle.addEventListener("mouseleave", function () {
+        hideTimeout = setTimeout(function () {
+            dropdownContent.style.opacity = "0";
+            dropdownContent.style.visibility = "hidden";
+            setTimeout(() => {
+                dropdownContent.style.display = "none";
+            }, 300);
+        }, 500);
+    });
+
+    dropdownContent.addEventListener("mouseleave", function () {
+        hideTimeout = setTimeout(function () {
+            dropdownContent.style.opacity = "0";
+            dropdownContent.style.visibility = "hidden";
+            setTimeout(() => {
+                dropdownContent.style.display = "none";
+            }, 300);
+        }, 500);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.querySelector(".sub-dropdown-toggle");
+    const submenu = document.querySelector(".sub-dropdown-content");
+
+    let hideTimeout;
+
+    function showSubmenu() {
+        clearTimeout(hideTimeout); 
+        submenu.style.display = "block";
+    }
+
+    function hideSubmenu() {
+        hideTimeout = setTimeout(() => {
+            submenu.style.display = "none";
+        }, 300); 
+    }
+
+    toggle.addEventListener("mouseenter", showSubmenu);
+    submenu.addEventListener("mouseenter", showSubmenu);
+
+    toggle.addEventListener("mouseleave", hideSubmenu);
+    submenu.addEventListener("mouseleave", hideSubmenu);
+});
