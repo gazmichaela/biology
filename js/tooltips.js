a6809387af3a2c157803ea803afb1449b37e1243
gazmichaela <michaela09g@gmail.com>
2026-03-30 14:08:38 +0200

    tooltipy

--- js/tooltips.js
+++ js/tooltips.js
@@ -46,11 +46,25 @@
       const tooltipText = tooltip.querySelector(".tooltiptext");
 
       if (tooltipText) {
-        if (window.innerWidth > 768) {
-          tooltipText.style.left = "50%";
-          tooltipText.style.transform = "translateX(-50%)";
-          return;
-        }
+     if (window.innerWidth > 768) {
+  tooltipText.style.left = "50%";
+  
+  requestAnimationFrame(() => {
+    const rect = tooltipText.getBoundingClientRect();
+    const clientWidth = document.documentElement.clientWidth;
+    
+    if (rect.right > clientWidth - 10) {
+      const overflow = rect.right - clientWidth + 10;
+      tooltipText.style.transform = `translateX(calc(-50% - ${overflow}px))`;
+    } else if (rect.left < 10) {
+      const overflow = 10 - rect.left;
+      tooltipText.style.transform = `translateX(calc(-50% + ${overflow}px))`;
+    } else {
+      tooltipText.style.transform = "translateX(-50%)";
+    }
+  });
+  return;
+}
 
         tooltipText.style.transition = "none !important";
         tooltipText.style.left = "0";
@@ -69,5 +83,4 @@
     }
   });
 })();
-
 /* (tento script používá formátování prettier) */
